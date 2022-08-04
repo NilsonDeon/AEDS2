aae93777187458553a548d093ea54ad1d73d2b34
Nilson Deon <NilsonDeon01@gmail.com>
2022-08-04 11:19:44 -0300

    q02-remade

--- labs/lab01/q02.java
+++ labs/lab01/q02.java
@@ -1,7 +1,8 @@
 import java.text.Normalizer;
 import java.util.Scanner;
 
-public class q02 {
+
+class q02 {
 
     public static int isMaiuscula(String s, int index){
 
@@ -9,32 +10,43 @@ public class q02 {
 
             if (s.charAt(index) >= 'A' && s.charAt(index) <='Z') { return 1 + isMaiuscula(s, index+1);}
 
-            if (s.charAt(index) >= 'a' && s.charAt(index) <='z' || s.charAt(index) == ' ') { return 0 + isMaiuscula(s, index+1);}
+            if (s.charAt(index) >= 'a' && s.charAt(index) <='z' || s.charAt(index) >= ' ' && s.charAt(index) <= '@') { 
+                return 0 + isMaiuscula(s, index+1);
+            }
         }
 
         return 0;
     }
 
     public static void main(String[] args){
-        
-        try (Scanner input = new Scanner(System.in)) {
-            do {
 
+        String palavras[] = new String[1000];
+        int cont = 0;
+
+        try (Scanner input = new Scanner(System.in)) {
+            while (true){
+                
                 String string = input.nextLine();
 
                 string = Normalizer.normalize(string, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", ""); // remove carcteres especiais
 
                 if ( (string.length() == 3 && string.charAt(0) == 'F' && string.charAt(1) == 'I' && string.charAt(2) == 'M') == false) { // para loop quando esquever FIM
 
-                    int resp = isMaiuscula(string, 0);
+                    palavras[cont] = string;
 
-                    System.out.println(resp);
+                    cont++;
                 }
                 else{ break; }
 
-            } while (true);
+            };
+        }
+
+        for (String palavra : palavras) {
+            int retorno = isMaiuscula(palavra, 0);
+            System.out.println(retorno);
         }
  
     }
 
 }
+
